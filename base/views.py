dfa0500b14ad0c0b8e33ce3b2fbde0b1fa0bfa79
Goodnessmbakara <amicablembakara50@gmail.com>
2022-09-06 23:21:21 +0100

    functionalities: updated CRUD

--- base/views.py
+++ base/views.py
@@ -13,7 +13,7 @@ def home(request):
 def room(request, pk):
     RoomObj=Room.objects.get(id=pk)
     context = {'RoomObj':RoomObj}
-    return render (request,"base/room.html",context=context)
+    return render(request,"base/room.html",context=context)
 
 def createRoom(request):
     form = RoomForm
@@ -21,8 +21,28 @@ def createRoom(request):
         form = RoomForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('/room/')
+            return redirect('home')
             
         
     context = {'forms':form}
     return render (request,'base/SignUp.html',context)
+
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room) #prefilled form
+    if request.method == 'POST':
+        form = RoomForm(request.POST,instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {'forms':form}
+    return render (request,'base/SignUp.html',context)
+
+def deleteRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect ('home')
+    context={'room':room}
+    return render (request, 'base/delete.html',context)
+    
